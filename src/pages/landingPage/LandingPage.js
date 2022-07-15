a20f7ec5ea63a7aa49342647b3a11891f7863783
Michael Z <michaelzwierzanski@gmail.com>
2022-07-15 19:40:14 +0100

    #14 added landingPage and react router dom

---
+++ src/pages/landingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Splash from '../../components/landingPage/splash/Splash'
+
+const LandingPage = () => {
+  return (
+    <>
+        <div>LandingPage</div>
+        <Splash />
+    </>
+  );
+};
+
+export default LandingPage;
