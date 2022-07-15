3189ae5e8ca86cdd514d90a1f35f11cac5bf0ec0
Michael Z <michaelzwierzanski@gmail.com>
2022-07-15 20:20:25 +0100

    #14 footer added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Routes, Route } from 'react-router-dom';
 
 import Header from "./components/UI/header/Header";
+import Footer from "./components/UI/footer/Footer";
 
 import LandingPage from "./pages/landingPage/LandingPage";
 
@@ -25,6 +26,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
