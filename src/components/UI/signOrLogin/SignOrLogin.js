4fa3176d61eb620324dae1c53185548014b7c641
Michael Z <michaelzwierzanski@gmail.com>
2022-07-15 20:13:07 +0100

    #14 test function added

--- src/components/UI/signOrLogin/SignOrLogin.js
+++ src/components/UI/signOrLogin/SignOrLogin.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useEffect } from 'react';
+
+import { testFunction } from '../../../functions/signupOrLogin/helpers/helpers';
 
 const SignOrLogin = () => {
+
+  useEffect(() => {
+    testFunction();
+  }, []);
+
   return (
     <div>SignOrLogin</div>
   );
