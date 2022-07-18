7850a2fba5e423adc57d09c21a239c98ba48ad13
Stalius4 <deivydasrimgaila@gmail.com>
2022-07-18 16:22:41 +0100

    #31 background image added

--- src/pages/landingPage/LandingPage.js
+++ src/pages/landingPage/LandingPage.js
@@ -1,13 +1,12 @@
 import React from 'react';
-
-
+import "./LandingPage.css"
 
 const LandingPage = () => {
   return (
-    <>
+    <div className='landing-page-main'>
         <div>LandingPage</div>
-        <p>Splash component removed, splash area native to this page.</p>
-    </>
+        <p>Splash  component removed, splash area native to this page.</p>
+    </div>
   );
 };
 
