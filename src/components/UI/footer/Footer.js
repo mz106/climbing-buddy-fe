fb72b1af358fe6765f31415b7d71970db89c5b7a
wumpfroot <markuskojo@msn.com>
2022-07-18 20:18:54 +0100

    #25 Basic design ready + footer responsive

--- src/components/UI/footer/Footer.js
+++ src/components/UI/footer/Footer.js
@@ -1,47 +1,51 @@
 import React from 'react';
+import './CSS/Footer.css'
+import './CSS/footer_mq.css'
 import { FiTwitter, FiInstagram, FiYoutube } from 'react-icons/fi'
 import { AiOutlineLinkedin } from 'react-icons/ai'
 
 const Footer = () => {
   return (
     <footer className='main-footer'>
-      <section className='footer-navigation'>
-        <nav>
-          <ul>
-            <li>About</li>
-            <li>Events</li>
-            <li>News</li>
-          </ul>
-        </nav>
-        <div className="vl"></div>
-        <nav>
-          <ul>
-            <li>Routes</li>
-            <li>Community</li>
-            <li>Get Started</li>
-          </ul>
-        </nav> 
-      </section>
-      <section className='footer-logo'>
-        <img src="" alt="Footer-logo" />
-      </section>
-      <section className='footer-socials'>
-        <h2>Follow Us</h2>
-          <FiTwitter />
-          <FiInstagram />
-          <FiYoutube />
-          <AiOutlineLinkedin />
+      <section className="footer-top">
+      <div className='footer-logo'>
+          <h1>ClimbingBuddy</h1>
+        </div>
+        <div className='footer-navigation'>
+          <nav>
+            <ul>
+              <li>About</li>
+              <li>Events</li>
+              <li>News</li>
+            </ul>
+          </nav>
+          <div className="vl"></div>
+          <nav>
+            <ul>
+              <li>Routes</li>
+              <li>Community</li>
+              <li>Get Started</li>
+            </ul>
+          </nav> 
+        </div>
+        <div className='footer-socials'>
+          <h2>Follow Us</h2>
+            <FiTwitter />
+            <FiInstagram />
+            <FiYoutube />
+            <AiOutlineLinkedin />
+        </div>
       </section>
       <section className="footer-bottom">
-        <i>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam perferendis porro ipsa magnam vitae deserunt natus perspiciatis illo eveniet quo quis labore repellat nostrum rem eaque quam, hic unde quibusdam?</i>
-        <div className="footer-contact">
+      <div className="footer-contact">
           <h2>Contact Us</h2>
           <p className='footer-contact-details'>
-            phone: 019209201290
-            email: bbuddy@climb.com
-            adress: 100 Some Address, Manchester, UK
+            phone: 019209201290<br></br>
+            email: buddy@climb.com<br></br>
+            address: 100 Some Address, Manchester, UK
           </p>
         </div>
+        <i>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</i>
         {/* Some legal text and links here */}
       </section>
     </footer>
