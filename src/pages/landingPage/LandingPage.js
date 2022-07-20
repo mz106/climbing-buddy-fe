4c1f14c19ed0b5f19b7694e3f2649ad5b768ef14
Stalius4 <deivydasrimgaila@gmail.com>
2022-07-20 14:39:44 +0100

    31 landing page design added

--- src/pages/landingPage/LandingPage.js
+++ src/pages/landingPage/LandingPage.js
@@ -3,13 +3,14 @@ import "./LandingPage.css"
 
 const LandingPage = () => {
   return (
-     
+     <div className='landing-page-background'>
     <div className='landing-page-main'>
+      
         <button className='landing-page-button'>Find your buddy</button>
         <h1 className='landing-page-title'>Climbing buddy</h1>
         <p className='landing-page-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt tempora veritatis est recusandae </p>
     </div>
- 
+    </div>
   );
 };
 
