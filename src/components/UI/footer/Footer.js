5de8186e671e73118e0b21f9f478a68d8bab6b8e
wumpfroot <markuskojo@msn.com>
2022-07-28 03:33:39 +0100

    #25 Added font + contact icons + hover states + small stuff

--- src/components/UI/footer/Footer.js
+++ src/components/UI/footer/Footer.js
@@ -1,9 +1,8 @@
-import React from 'react';
 import './CSS/Footer.css';
-import './CSS/footer_mq.css';
 import { FiTwitter, FiInstagram, FiYoutube } from 'react-icons/fi';
-import { AiOutlineLinkedin } from 'react-icons/ai';
-import { BsArrowUpCircle } from 'react-icons/bs';
+import { AiOutlineLinkedin, AiOutlineMail } from 'react-icons/ai';
+import { BsArrowUpCircle, BsTelephone } from 'react-icons/bs';
+import { FaRegAddressBook } from 'react-icons/fa'
 
 const Footer = () => {
   return (
@@ -41,16 +40,16 @@ const Footer = () => {
       <div className="footer-contact">
           <h2>Contact Us</h2>
           <p className='footer-contact-details'>
-            phone: 019209201290<br></br>
-            email: buddy@climb.com<br></br>
-            address: 100 Some Address, Manchester, UK
+            <BsTelephone /> 019209201290<br></br>
+            <AiOutlineMail /> buddy@climb.com<br></br>
+            <FaRegAddressBook /> 100 Some Address, Manchester, UK
           </p>
         </div>
         <i>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</i>
         {/* Some legal text and links here */}
 
         {/* Back to top icon */}
-        <BsArrowUpCircle size={50}/>
+        <BsArrowUpCircle className='back-to-top-icon' size={50}/>
       </section>
     </footer>
   );
