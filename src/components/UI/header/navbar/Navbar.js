7c3b4f061b3fd0adefbb5013a566b5e22e830cc5
Stalius4 <deivydasrimgaila@gmail.com>
2022-07-29 11:32:10 +0100

    mobile responsive added

--- src/components/UI/header/navbar/Navbar.js
+++ src/components/UI/header/navbar/Navbar.js
@@ -1,22 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./navbar.css"
 import logo from "./logo.png"
 const Navbar = () => {
+
+  const [navBarSwich, setNavBarSwich] = useState (false)
+
   return (
     <div className='Navbar'>
       <div className='Navbar-inner'>
      
-    <ul className='Navbar-list'>
-      <li className='Navbar-profile white'>Profile</li>
-      <li className='Navbar-home white'>Home</li>
-      <li > <img className='Navbar-logo' src={logo} alt="logo" height={70} /></li>
-      <li className='Navbar-login white'>Login</li>
-      <li className='Navbar-register white'>Register</li>
-    </ul>
+   {navBarSwich ? <div className='Navbar-flex '>
+      <button className='Navbar-profile btn'>Profile</button>
+      <button className='Navbar-home btn'>Home</button>
+      </div> : <div></div>} 
+       <img className='Navbar-logo' src={logo} alt="logo" height={70} />
+       <div className='Navbar-flex Login-register-btns'>
+      <button className='Navbar-login btn'>Login</button>
+      <button className='Navbar-register btn'>Register</button>
+      </div>
+    </div>
    
    
     </div>
-    </div>
+ 
   )
 }
 
